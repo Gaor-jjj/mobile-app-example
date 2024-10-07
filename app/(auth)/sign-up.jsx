b40ab4d7eff944e03048a13e96913fb06af8994f
Gaor-jjj <kaurtamm1995@gmail.com>
2024-10-07 23:35:13 +0300

    Sign up footer

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import AuthHeader from '../../components/AuthHeader'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -14,38 +14,41 @@ const SignUp = () => {
 
   return (
     <SafeAreaView className="h-full bg-white">
-        <View className="w-full h-full p-5">
-            <AuthHeader
-                title='Sign Up'
-                textStyles='font-mbold text-[26px]'
-                handlePress={() => router.back()}
-                containerStyles="mb-[54px]"
-            />
-            <InputField
-                label='Name'
-                placeholder='John Doe'
-            />
-            <InputField
-                label='Email'
-                placeholder='example@gmail.com'
-            />
-            <InputField
-                label='Password'
-                placeholder='*******'
-                isPassword
-            />
-            <View className="flex-row items-center">
-                <Checkbox checked={checked} onCheck={setChecked}/>
-                <Text className="text-primary mx-3">I agree with <Text className="font-mbold">Terms & Privacy</Text></Text>
+        <ScrollView>
+            <View className="w-full h-full p-5">
+                <AuthHeader
+                    title='Sign Up'
+                    textStyles='font-mbold text-[26px]'
+                    handlePress={() => router.back()}
+                    containerStyles="mb-10"
+                />
+                <InputField
+                    label='Name'
+                    placeholder='John Doe'
+                />
+                <InputField
+                    label='Email'
+                    placeholder='example@gmail.com'
+                />
+                <InputField
+                    label='Password'
+                    placeholder='*******'
+                    isPassword
+                />
+                <View className="flex-row items-center">
+                    <Checkbox checked={checked} onCheck={setChecked}/>
+                    <Text className="text-primary mx-3">I agree with <Text className="font-mbold">Terms & Privacy</Text></Text>
+                </View>
+                <CustomButton
+                    title='Sign Up'
+                    handlePress={() => router.push('/')}
+                    containerStyles='my-5'
+                />
+                <Seperator text='Or sign up with'/>
+                <GoogleLogin/>
+                <Text className="font-mregular text-primary text-center mt-10 pb-5">Already have an account? <Text className="font-mbold">Sign In</Text></Text>
             </View>
-            <CustomButton
-                title='Sign In'
-                handlePress={() => router.push('/')}
-                containerStyles='mt-5'
-            />
-            <Seperator text='Or sign up with'/>
-            <GoogleLogin/>
-        </View>
+        </ScrollView>
     </SafeAreaView>
   )
 }
--- components/Seperator.jsx
+++ components/Seperator.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Seperator = ({text}) => {
   return (
-    <View className="flex-row items-center my-5">
+    <View className="flex-row items-center my-6">
       <View className="bg-lgray h-[1px] flex-1 mx-2"/>
       <Text className="text-primary font-mbold">{text}</Text>
       <View className="bg-lgray h-[1px] flex-1 mx-2"/>
