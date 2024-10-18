144e8c2ca7ab1b517e90d56a9386f33a7805e545
gaor-jjj <kaurtamm1995@gmail.com>
2024-10-18 14:50:28 +0300

    Search box for Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,35 +1,48 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import icons from '../constants/icons'
+import InputField from './InputField'
 
-const Header = ({ title, showBack, showLogout, showSearch, onBackPress, onLogoutPress, onSearchPress }) => {
+const Header = ({ title, showBack, showLogout, showSearch, onBackPress, onLogoutPress }) => {
+  const [showSearchInput, setShowSearchInput] = useState(false)
+  const toggleSearch = () => {
+    setShowSearchInput(!showSearchInput);
+  }
+
   return (
-    <View className="flex-row items-center justify-between w-full">
+    <View className="w-full">
+      <View className="flex-row items-center justify-between">
 
-      {/* Left Side: Either Back or Search */}    
-      <View className="w-10">
-        {showBack ? (
-            <TouchableOpacity onPress={onBackPress}>
-                <Image source={icons.back_simple} className="w-6 h-6"/>
-            </TouchableOpacity>
-        ) : showSearch ? (
-            <TouchableOpacity onPress={onSearchPress}>
-                <Image source={icons.search} className="w-6 h-6"/>
-            </TouchableOpacity>
-        ) : null}
+        {/* Left Side: Either Back or Search */}    
+        <View className="w-10">
+          {showBack ? (
+              <TouchableOpacity onPress={onBackPress}>
+                  <Image source={icons.back_simple} className="w-6 h-6"/>
+              </TouchableOpacity>
+          ) : showSearch ? (
+              <TouchableOpacity onPress={toggleSearch}>
+                  <Image source={icons.search} className="w-6 h-6"/>
+              </TouchableOpacity>
+          ) : null}
+        </View>
+
+        {/* Title */}      
+        <Text className="font-mbold text-[16px]">{title}</Text>
+
+        {/* Right Side: Logout */}      
+        <View className="w-10">
+          {showLogout && (
+              <TouchableOpacity onPress={onLogoutPress}>
+                  <Image source={icons.logout} className="w-6 h-6"/>
+              </TouchableOpacity>
+          )}
+        </View>
       </View>
-
-      {/* Title */}      
-      <Text className="font-mbold text-[16px]">{title}</Text>
-
-      {/* Right Side: Logout */}      
-      <View className="w-10">
-        {showLogout && (
-            <TouchableOpacity onPress={onLogoutPress}>
-                <Image source={icons.logout} className="w-6 h-6"/>
-            </TouchableOpacity>
-        )}
-      </View>
+      {showSearchInput && (
+        <View className="mt-2">
+          <InputField placeholder={`Type your keyword`}/>
+        </View>
+      )}
     </View>
   )
 }
